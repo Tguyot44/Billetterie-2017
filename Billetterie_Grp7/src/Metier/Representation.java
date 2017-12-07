3796a4a8d7eed3874c5d272f9c6c47e6d7b10262
qbaudry <qbaudry@btssio-VirtualBox>
2017-12-07 14:37:00 +0100

    Création des classes Métiers Groupe, Lieu et Representation

---
+++ Billetterie_Grp7/src/Metier/Representation.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metier;
+
+import java.util.Date;
+
+/**
+ *
+ * @author qbaudry
+ */
+
+public class Representation {
+    
+    private String idRep;
+    private Date dateRep;
+    private String leLieu;
+    private String leGroupe;
+    private String heureDebut;
+    private String heureFin;
+    
+    public Representation(String idRep, Date dateRep, String leLieu, String leGroupe, String heureDebut, String heureFin) {
+        this.idRep = idRep;
+        this.dateRep = dateRep;
+        this.leLieu = leLieu;
+        this.leGroupe = leGroupe;
+        this.heureDebut = heureDebut;
+        this.heureFin = heureFin;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Representation{" + "id=" + idRep + ", date=" + dateRep + ", lieu=" + leLieu + ", groupe=" + leGroupe + ", heure_debut=" + heureDebut + ", heure_fin=" + heureFin + '}';
+    }
+
+    public String getIdRep() {
+        return idRep;
+    }
+
+    public void setIdRep(String idRep) {
+        this.idRep = idRep;
+    }
+
+    public Date getDateRep() {
+        return dateRep;
+    }
+
+    public void setDateRep(Date dateRep) {
+        this.dateRep = dateRep;
+    }
+
+    public String getLeLieu() {
+        return leLieu;
+    }
+
+    public void setLeLieu(String leLieu) {
+        this.leLieu = leLieu;
+    }
+
+    public String getLeGroupe() {
+        return leGroupe;
+    }
+
+    public void setLeGroupe(String leGroupe) {
+        this.leGroupe = leGroupe;
+    }
+
+    public String getHeureDebut() {
+        return heureDebut;
+    }
+
+    public void setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+    }
+
+    public String getHeureFin() {
+        return heureFin;
+    }
+
+    public void setHeureFin(String heureFin) {
+        this.heureFin = heureFin;
+    }
+    
+}
