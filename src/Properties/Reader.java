87e40793ce6f0de7516f2d916a4b702bed9805ef
ttnguyen <ttnguyen@btssio-VirtualBox>
2018-03-15 17:05:34 +0100

    modif connexion bdd

--- src/Properties/Reader.java
+++ src/Properties/Reader.java
@@ -13,6 +13,8 @@ public class Reader {
     public static String readString(String searchedString) {
         Properties pReader = new Properties();
         try {
+            System.out.println(pReader);
+            System.out.println(Reader.class.getResourceAsStream("Billetterie.properties"));
             pReader.load(Reader.class.getResourceAsStream("Billetterie.properties"));
         } catch (Exception e) {
             throw new RuntimeException(e);
